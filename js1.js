cdd7a816effe6bea8b7e53008ddc514728c7757b
DanLuo <dluo1019@gmail.com>
2015-05-06 23:11:00 -0400

    modification

--- js1.js
+++ js1.js
@@ -1,7 +1,7 @@
 //var names=new Array();
 //$.get('info.txt',function(data){
 //names=data.split(',');});
-var names=['eee','rrr','ttt','qqq'];
+var names=['eee','rrr','ttt','qqq','ppp','sss'];
 var Name;
 var name;
 var N=1;
@@ -17,16 +17,15 @@ document.getElementById("name").innerHTML=name;
 
 stop=function()
 {clearInterval(Name);
- if (l!=1){
 var Ni="n"+N;
 document.getElementById(Ni).innerHTML=name;
 var index=names.indexOf(name);
 names.splice(index,1);
 Ns[N-1]=Ni;
-N=N+1;}
-else
+N=N+1;
+if (l<=2)
 {document.getElementById("nn").innerHTML="The list is full";}
 };
 
-refresh=function()
-{parent.removeChild(
+
+
