084262652f7861c0b62cd61d400e7359bea4c79b
DanLuo <dluo1019@gmail.com>
2015-05-08 13:19:44 -0400

    modification

--- js1.js
+++ js1.js
@@ -2,10 +2,12 @@
 //$.get('info.txt',function(data){
 //names=data.split(',');});
 var names=['eee','rrr','ttt','qqq','ppp','sss'];
+var roles=['vocal','guitar','drum','bass','keyboard'];
 var Name;
 var name;
 var N=1;
 var Ns=[];
+var l;
 getname=function(){
 Name=setInterval(function(){pickname()},50);};
 pickname=function(){
@@ -18,17 +20,16 @@ document.getElementById("name").innerHTML=name;
 stop=function()
 {clearInterval(Name);
 var Ni="n"+N;
+var role=roles[N-1];
 document.getElementById(Ni).innerHTML=name;
+document.getElementById(role).value=name;
 var index=names.indexOf(name);
 names.splice(index,1);
-Ns[N-1]=Ni;
+//Ns[N-1]=Ni;
 N=N+1;
 if (l<=2)
 {document.getElementById("nn").innerHTML="The list is full";}
 };
 
 
-<<<<<<< Updated upstream
 
-=======
->>>>>>> Stashed changes
